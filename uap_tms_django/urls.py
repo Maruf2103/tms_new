456c034404a0ab0a60992a11973568981cf7e458
Samia Zaman <samiazaman174@gmail.com>
2025-10-23 10:31:25 +0600

    transportation/urls.py         uap_tms_django/

---
+++ uap_tms_django/urls.py
@@ -0,0 +1,13 @@
+# uap_tms_django/urls.py
+from django.contrib import admin
+from django.urls import path, include
+from transportation import views as transport_views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('admin-panel/', include('custom_admin.urls')),
+    path('', transport_views.home, name='home'),
+    path('login/', transport_views.user_login, name='login'),
+    path('signup/', transport_views.signup, name='signup'),
+    path('transport/', include('transportation.urls')),
+]
